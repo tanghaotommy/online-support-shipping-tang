9bf895bb7aaf384f28f4e9166b9724232aa7572c
汤豪 <tanghao@s-MacBook-Pro.local>
2017-01-08 19:00:41 -0800

    2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import urllib
 import json
 import os
+import requests
 
 from flask import Flask
 from flask import request
@@ -27,20 +28,103 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-@app.route('/smarthome', methods=['POSTs'])
+@app.route('/smarthome', methods=['POST'])
 def smarthome():
     req = request.get_json(silent=True, force=True)
 
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
+    res = makeResponse(req)
 
     res = json.dumps(res, indent=4)
     print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
+
+def makeResponse(req):
+	action = req.get("result").get("action")
+	result = req.get("result")
+	parameters = result.get("parameters")
+	res = {}
+
+	if action == "action.openfrontdoor":
+		print("Open Front Door!")
+		content = {
+		"Type": "OpenDoor"
+		}
+		r = requests.post("http://localhost/order.php", data=json.dumps(content))
+    	speech = "Your front door is opened!"
+
+	if action == "status.all":
+		print("Check current status")
+		content = {
+		"Type": "HomeStatus"
+		}
+		r = requests.post("http://localhost/order.php", data=json.dumps(content))
+		if r.json().get("Status") == 0:
+			speech = "Your home is all right!"
+			res["data"] = r.json().get("Content")
+		else:
+			speech = "Sorry, I meet some errors. Please try again later!"
+
+	if action == "action.turnofflights":
+		location = parameters.get("Location")
+		if location == "":
+			content = {
+			"Type": "TurnOffLight",
+			}
+			r = requests.post("http://localhost/order.php", data=json.dumps(content))
+			if r.json().get("Status") == 0:
+				print("Turn off the lights!")
+				speech = "I have turned off the lights!"
+			else:
+				speech = "Sorry, I meet some errors. Please try again later!"
+		else:
+			content = {
+			"Type": "TurnOffLight",
+			"Location": location
+			}
+			r = requests.post("http://localhost/order.php", data=json.dumps(content))
+			if r.json().get("Status") == 0:
+				print("Turn off the lights!" + location)
+				speech = "I have turned off the lights in the " + location + " !"
+			else:
+				speech = "Sorry, I meet some errors. Please try again later!"
+
+	if action == "action.turnonlights":
+		location = parameters.get("Location")
+		if location == "":
+			content = {
+			"Type": "TurnOnLight",
+			}
+			r = requests.post("http://localhost/order.php", data=json.dumps(content))
+			if r.json().get("Status") == 0:
+				print("Turn on the lights!")
+				speech = "I have turned on the lights!"
+			else:
+				speech = "Sorry, I meet some errors. Please try again later!"
+		else:
+			content = {
+			"Type": "TurnOnLight",
+			"Location": location
+			}
+			r = requests.post("http://localhost/order.php", data=json.dumps(content))
+			if r.json().get("Status") == 0:
+				print("Turn on the lights!" + location)
+				speech = "I have turned on the lights in the " + location + " !"
+			else:
+				speech = "Sorry, I meet some errors. Please try again later!"
+
+	print("Response:")
+	print(speech)
+
+	res["speech"] = speech
+	res["displayText"] = speech
+	res["source"] = "apiai-onlinestore-shipping"
+
+	return res;
 
 def makeWebhookResult(req):
     if req.get("result").get("action") != "shipping.cost":
@@ -70,4 +154,4 @@ if __name__ == '__main__':
 
     print "Starting app on port %d" % port
 
-    app.run(debug=True, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
