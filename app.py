ebe25e81e6440ba2f8b3d2f57c7bb9dc2abfd9b9
汤豪 <tanghao@s-MacBook-Pro.local>
2017-02-05 18:50:49 -0800

    123

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def restaurantsRec():
 
     res = makeResponse2(req)
 
-    res = json.dumps(res, indent=4, ensure_ascii=False)
+    res = json.dumps(res, indent=4)
     print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
